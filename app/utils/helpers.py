f8137d346715711b10b32e31c9abcc90703b4bd2
jguruprasathwebmdnet <jguruprasath@webmd.net>
2025-05-29 13:16:24 +0530

    Create helpers.py

---
+++ app/utils/helpers.py
@@ -0,0 +1,5 @@
+def parse_email_content(raw_email):
+    # Placeholder for parsing raw email input
+    subject = raw_email.get("subject", "")
+    body = raw_email.get("body", "")
+    return subject, body
